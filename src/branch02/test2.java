29642421b8d737c2f8d006362b351cb92dc32c81
allen <jianlai2007@qq.com>
2017-12-12 11:24:06 +0800

    学习branch

---
+++ src/branch02/test2.java
@@ -0,0 +1,16 @@
+package branch02;
+
+/**
+ * Created by Allen on 2017/12/12.
+ */
+public class test2 {
+    private String test2;
+
+    public String getTest2() {
+        return test2;
+    }
+
+    public void setTest2(String test2) {
+        this.test2 = test2;
+    }
+}
